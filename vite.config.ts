18de853ae435d17d6fe2d748c01e985879b003d1
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2025-01-16 22:50:49 -0500

    add jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/__tests__/setup.js'],
+  },
   server: {
     port: 3000
   }
